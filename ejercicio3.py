c57d4420405e5e53294268b3ab3f2d48c1463ffb
Franlop24 <franlopbri@gmail.com>
2024-10-14 10:22:00 -0600

    Implementa math.sqrt en ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -11,9 +11,11 @@ Salidas:
 Análisis:
     Se utiliza el teorema de pitágoras
 '''
+import math
 a = input("Escribe el valor del cateto A: ")
 a = int(a)
 b = input("Escribe el valor del cateto B: ")
 b = int(b)
 c = (a * a + b * b) ** (1 / 2)
+c = math.sqrt(a * a + b * b)
 print("El valor de la hipotenusa es:", c)
