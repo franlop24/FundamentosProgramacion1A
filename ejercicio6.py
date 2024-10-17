d9af26f16df911f219987670ab15ca4393fe36fc
Franlop24 <franlopbri@gmail.com>
2024-10-17 13:37:58 -0600

    Ejercicio 6

---
+++ ejercicio6.py
@@ -0,0 +1,15 @@
+'''
+Ejercicio 6:
+    Calcular la media de 3 números
+Entradas:
+    num1: int
+    num2: int
+    num3: int
+Salidas:
+    media: float
+'''
+num1 = int(input('Ingresa el Número 1: '))
+num2 = int(input('Ingresa el Número 2: '))
+num3 = int(input('Ingresa el Número 3: '))
+media = (num1 + num2 + num3) / 3
+print("La media de los 3 numeros es:", media)
